83424133f7dc3446434f5404b85a4b280149bd03
Julia-Wag <juli.w.081009@gmail.com>
2026-05-27 13:59:08 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public static int contador = 0;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            contador++;
+
+            Debug.Log("Objetos recolectados: " + contador);
+
+            Destroy(gameObject);
+        }
+    }
+}
