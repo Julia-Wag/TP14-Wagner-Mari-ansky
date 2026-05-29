b7665d14fc0ac6008db4d3a59196d8fd677e8888
Julia-Wag <juli.w.081009@gmail.com>
2026-05-29 08:46:35 -0300

    pt 3 terminada

---
+++ Assets/Scripts/InteractibleArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleArea : MonoBehaviour
+{
+   public int score = 0;
+
+    private UIManager uiManager;
+
+    private void Awake()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Lapicera"))
+        {
+            score++;
+
+            uiManager.UpdateScore(score);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+     public float timer = 0f;
+
+    private UIManager uiManager;
+
+    private void Start()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        uiManager.UpdateTimer(timer);
+    }
+}
